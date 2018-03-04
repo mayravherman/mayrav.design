a0bbfce90c2b032578a18b3bc9e7dd414c22ddaa
Mayrav Herman <mayravherman@gmail.com>
2018-03-04 14:09:10 -0800

    container

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import { css } from 'emotion';
-import { Container } from 'rebass';
 
 import { sizes } from '../utils/media';
 
 export const container = css`
   padding: 1.5rem 2.5rem;
   transition: all 0.4s ease-in-out;
+  max-width: 68rem;
+  margin-left: auto;
+  margin-right: auto;
   @media (max-width: ${sizes.small}px) {
     padding: 0.5rem 1.25rem;
   }
 `;
 
 export default ({ children }) => (
-  <Container
-    is="main"
-    px={[5, 5, 8]}
-    pt={[2, 2, 5]}
-    pb={2}
-    maxWidth="68rem"
-    className={container}
-  >
+  <main className={container}>
     {children}
-  </Container>
+  </main>
 );
+
