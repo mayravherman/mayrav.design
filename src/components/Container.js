d508ab7307a69bfc02a9c01af1a0f59d1fee6292
Mayrav Herman <mayravherman@gmail.com>
2018-03-04 13:59:13 -0800

    main container

--- src/components/Container.js
+++ src/components/Container.js
@@ -15,8 +15,8 @@ export const container = css`
 export default ({ children }) => (
   <Container
     is="main"
-    px={[5, 5, 8, 8, 8, 0]}
-    pt={[2, 5]}
+    px={[5, 5, 8, 8, 0]}
+    pt={[2, 2, 5]}
     pb={2}
     maxWidth="68rem"
     className={container}
--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,8 +1,8 @@
-const breakpoints = [ '36em', '48em', '62em', '75em', '80em', '100em' ]
+const breakpoints = ['36em', '48em', '62em', '75em', '80em', '100em'];
 
-const fontSizes = [12, 14, 16, 18, 20, 24, 32, 36, 44, 64, 72, 96]
+const fontSizes = [12, 14, 16, 18, 20, 24, 32, 36, 44, 64, 72, 96];
 
-const space = [0, 4, 8, 12, 16, 20, 24, 32, 40, 64, 128]
+const space = [0, 4, 8, 12, 16, 20, 24, 32, 40, 64, 128];
 
 const colors = {
   purple: '#29007A',
@@ -14,10 +14,10 @@ const colors = {
   yellow: '#FFF63C',
   grey: '#C9C8CC',
   lightGrey: '#F9F9F9',
-}
+};
 
 const theme = {
-  breakpoints: [36, 75, 80, 100],
+  breakpoints: [32, 36, 48, 64, 80, 100],
   space: [0, 4, 8, 12, 16, 20, 24, 32, 40, 64, 128],
   fontSizes: [12, 14, 16, 18, 20, 24, 32, 36, 44, 64, 72, 96],
   colors: {
